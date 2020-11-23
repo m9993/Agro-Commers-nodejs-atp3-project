d8de9ea3e8681b70a72c3f1c5c3d610182fb646c
m9993 <muntasiralam9993@gmail.com>
2020-11-23 13:58:11 +0600

    final db

--- agro-commers.sql
+++ agro-commers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 20, 2020 at 10:30 PM
+-- Generation Time: Nov 23, 2020 at 08:56 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.1.33
 
@@ -78,15 +78,22 @@ CREATE TABLE `contact` (
 --
 
 INSERT INTO `contact` (`conid`, `sendermail`, `receivermail`, `conmessage`) VALUES
-(1, '1', '1', '1'),
-(2, '1', 'a@gmail.com', 'aaaa'),
-(3, '1', 'a@gmail.com', '123456'),
 (4, 'abc@gmail.com', 'a@gmail.com', 'hi there!'),
 (5, 'abc@gmail.com', 'a@gmail.com', '123'),
 (6, 'abc@gmail.com', 'a@gmail.com', 'aaaaa'),
 (7, 'abc@gmail.com', 'a@gmail.com', 'vvvv'),
-(8, 'abc@gmail.com', 'a@gmail.com', 'jkjk'),
-(9, 'abc@gmail.com', 'a@gmail.com', 'a1');
+(10, 'abc@gmail.com', 'smith@gmail.com', 'hi smith!'),
+(11, 'abc@gmail.com', 'john@gmail.com', 'q'),
+(12, 'john@gmail.com', 'abc@gmail.com', 'q'),
+(13, 'smith@gmail.com', 'abc@gmail.com', 'hi smith!'),
+(14, 'smith@gmail.com', 'abc@gmail.com', 'hi smith!'),
+(15, 'mina@gmail.com', 'abc@gmail.com', 'q'),
+(16, 'mina@gmail.com', 'abc@gmail.com', 'hiiiiiiiiiiiiii'),
+(17, 'mina@gmail.com', 'abc@gmail.com', 'I have a question'),
+(18, 'smith@gmail.com', 'abc@gmail.com', 'meet me ASAP!'),
+(19, 'smith@gmail.com', 'john@gmail.com', '123'),
+(20, 'abc@gmail.com', 'smith@gmail.com', 'hi smith!'),
+(21, 'alex@gmail.com', 'smith@gmail.com', 'hello!');
 
 -- --------------------------------------------------------
 
@@ -170,7 +177,32 @@ CREATE TABLE `invoice` (
 --
 
 INSERT INTO `invoice` (`invnumber`, `uid`, `orderdate`, `subtotal`, `shipmethod`) VALUES
-(1, 1, '1', 1, '1');
+(1, 1, '2020-11-21 07:21:34', 1200, 'bkash'),
+(2, 2, '12', 456, 'bkask'),
+(3, 2, '2020-11-22 08:49:06', 1200, 'a'),
+(4, 1, '2020-11-22 09:30:02', 50, 'Parcel shipping'),
+(5, 1, '2020-11-22 09:30:58', 40, 'Parcel shipping'),
+(6, 1, '2020-11-22 09:34:53', 10, 'Parcel shipping'),
+(7, 1, '2020-11-22 09:46:59', 340, 'Will take from office'),
+(8, 1, '2020-11-22 09:48:54', 90, 'Parcel shipping'),
+(9, 1, '2020-11-22 09:49:29', 90, 'Will take from office'),
+(10, 1, '2020-11-22 09:49:39', 100, 'Parcel shipping'),
+(11, 1, '2020-11-22 09:49:50', 50, 'Will take from office'),
+(12, 1, '2020-11-22 09:50:37', 20, 'Parcel shipping'),
+(13, 1, '2020-11-22 09:50:52', 50, 'Parcel shipping'),
+(14, 1, '2020-11-22 09:51:24', 50, 'Parcel shipping'),
+(15, 1, '2020-11-22 09:52:09', 150, 'Parcel shipping'),
+(16, 1, '2020-11-22 09:55:07', 160, 'Parcel shipping'),
+(17, 1, '2020-11-22 09:56:18', 330, 'Parcel shipping'),
+(18, 1, '2020-11-22 10:48:39', 290, 'Will take from office'),
+(19, 1, '2020-11-22 10:59:22', 120, 'Parcel shipping'),
+(20, 1, '2020-11-22 11:31:49', 20, 'Parcel shipping'),
+(21, 1, '2020-11-22 11:43:41', 50, 'Parcel shipping'),
+(22, 2, '2020-11-22 12:34:05', 100, 'Parcel shipping'),
+(23, 1, '2020-11-23 13:45:10', 50, 'Parcel shipping'),
+(24, 1, '2020-11-23 13:48:07', 60, 'Parcel shipping'),
+(25, 1, '2020-11-23 13:50:26', 270, 'Parcel shipping'),
+(26, 1, '2020-11-23 13:51:51', 10, 'Will take from office');
 
 -- --------------------------------------------------------
 
@@ -184,7 +216,7 @@ CREATE TABLE `items` (
   `iprice` int(200) NOT NULL,
   `idetails` varchar(200) NOT NULL,
   `istatus` varchar(200) NOT NULL,
-  `iimage` varchar(200) NOT NULL,
+  `iimage` varchar(500) NOT NULL,
   `catid` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -193,8 +225,10 @@ CREATE TABLE `items` (
 --
 
 INSERT INTO `items` (`iid`, `iname`, `iprice`, `idetails`, `istatus`, `iimage`, `catid`) VALUES
-(1, 'Cocunut', 50, 'very famous', 'available', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS10GBWw_c6LyxFXe4P41UTR2-FrJrjR7u5WQ&usqp=CAU', 1),
-(2, 'Seeds', 10, 'seeds', 'available', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnyPlpyLPNVJ5e6G1nEcpOnwL6wn_WNwsgMw&usqp=CAU', 1);
+(1, 'Cocunut', 50, 'very famous', 'not available', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS10GBWw_c6LyxFXe4P41UTR2-FrJrjR7u5WQ&usqp=CAU', 1),
+(2, 'Seeds', 10, 'seeds', 'available', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnyPlpyLPNVJ5e6G1nEcpOnwL6wn_WNwsgMw&usqp=CAU', 1),
+(3, 'Fertilizer', 50, 'Very fertile', 'available', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrZQQOL356za2ULz_8eghmZI3BcRJ1ZGPgNg&usqp=CAU', 1),
+(5, 'Soyabeans', 100, 'Healthy', 'not available', 'https://miro.medium.com/max/1000/0*ahe8XINOHYbtG_lb.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -212,7 +246,10 @@ CREATE TABLE `notice` (
 --
 
 INSERT INTO `notice` (`nid`, `notice`) VALUES
-(1, '1');
+(1, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type a'),
+(2, 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distrib'),
+(3, '\"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut '),
+(4, '\"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the gr');
 
 -- --------------------------------------------------------
 
@@ -232,7 +269,8 @@ CREATE TABLE `order` (
 --
 
 INSERT INTO `order` (`oid`, `ostatus`, `hid`, `uid`) VALUES
-(1, '1', 1, 1);
+(1, 'delivered', 1, 1),
+(3, 'pending', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -252,7 +290,10 @@ CREATE TABLE `shop` (
 --
 
 INSERT INTO `shop` (`shid`, `shname`, `cid`, `iid`) VALUES
-(1, 'abcShop', 1, 1);
+(1, 'abcShop', 1, 1),
+(2, 'abcShop', 1, 2),
+(3, 'abcShop', 1, 3),
+(4, 'Shop Soyabeans', 1, 5);
 
 -- --------------------------------------------------------
 
@@ -275,8 +316,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`uid`, `uname`, `uphone`, `umail`, `uaddress`, `upass`, `user_role`) VALUES
-(1, 'John', 2147483647, 'john@gmail.com', 'mirpur-2, dhaka, Bangladesh', '123', 'customer'),
-(2, 'Smith', 12345, 'smith@gmail.com', 'dhaka', '123', 'admin');
+(1, 'John', 123456789, 'john@gmail.com', 'dhaka, Bangladesh', '123', 'admin'),
+(2, 'Smith', 12345, 'smith@gmail.com', 'dhaka', '123', 'customer');
 
 --
 -- Indexes for dumped tables
@@ -357,6 +398,9 @@ ALTER TABLE `order`
 --
 ALTER TABLE `shop`
   ADD PRIMARY KEY (`shid`),
+  ADD UNIQUE KEY `iid_2` (`iid`),
+  ADD UNIQUE KEY `iid_3` (`iid`),
+  ADD UNIQUE KEY `iid_4` (`iid`),
   ADD KEY `cid` (`cid`),
   ADD KEY `iid` (`iid`);
 
@@ -386,7 +430,7 @@ ALTER TABLE `comment`
 -- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `conid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `conid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `employee`
@@ -410,37 +454,37 @@ ALTER TABLE `inventory`
 -- AUTO_INCREMENT for table `invoice`
 --
 ALTER TABLE `invoice`
-  MODIFY `invnumber` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `invnumber` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `iid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `iid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `notice`
 --
 ALTER TABLE `notice`
-  MODIFY `nid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `nid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `order`
 --
 ALTER TABLE `order`
-  MODIFY `oid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `oid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `shop`
 --
 ALTER TABLE `shop`
-  MODIFY `shid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `shid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `uid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `uid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
